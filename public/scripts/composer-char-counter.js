eff4741045288e35ae12b23ab8dee8ce002cff32
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-03-06 00:18:41 +0000

    changes keyup => on input, adds empty string to if()

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-  $("textarea").keyup(function() {
+  $("textarea").on('input',function() {
 
     const $counter = $(this).parent().siblings(".button-Counter").children(".counter");
 
     $counter.text(140 - $(this).val().length);
-    if ($counter.text() < 0) {
+    if ($counter.text() < 0 || $counter.text() === '') {
       return $(".counter").addClass('charLimit');
     }
     $(".counter").removeClass('charLimit');
